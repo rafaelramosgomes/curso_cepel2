30e53a537a726c6feb9de9280e9e8b567e36ff5b
cmicelifarias <cmicelifarias@users.noreply.github.com>
2022-10-06 11:20:04 -0300

    Update web2.py

--- web2.py
+++ web2.py
@@ -2,26 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-"""Gets and prints the spreadsheet's header columns
 
-Parameters
-----------
-file_loc : str
-    The file location of the spreadsheet
-print_cols : bool, optional
-    A flag used to print the columns to the console (default is False)
-
-Returns
--------
-list
-    a list of strings representing the header columns
-"""
 
 def soma(termo1,termo2):
 
-    return termo1+termo2
-
-
 """Gets and prints the spreadsheet's header columns
 
 Parameters
@@ -36,8 +20,28 @@ Returns
 list
     a list of strings representing the header columns
 """
+    
+    return termo1+termo2
+
+
 @app.route("/")
 def hello():
+    
+    """Gets and prints the spreadsheet's header columns
+
+Parameters
+----------
+file_loc : str
+    The file location of the spreadsheet
+print_cols : bool, optional
+    A flag used to print the columns to the console (default is False)
+
+Returns
+-------
+list
+    a list of strings representing the header columns
+"""
+
 
     return "<h1>Teste de segurança</h1>"
 
