bab9ee691719cc45f84df6497585ae923403d469
cmicelifarias <cmicelifarias@users.noreply.github.com>
2022-10-06 11:13:28 -0300

    Mudanca devido ao pylint

--- web2.py
+++ web2.py
@@ -2,12 +2,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
+'''
+
+funcao que soma dois elementos
+
+'''
+
 def soma(a,b):
+
     return a+b
 
 
-# funcao raiz do meu projeto
+'''
+
+funcao raiz da minha aplicacao web
+
+'''
+
 @app.route("/")
 def hello():
+    
     return "<h1>Teste de segurança</h1>"
 
