f79542513622e3c90cd40cbb8539397945185562
cmicelifarias <cmicelifarias@users.noreply.github.com>
2022-10-06 11:18:32 -0300

    docstrings

--- web2.py
+++ web2.py
@@ -2,23 +2,40 @@ from flask import Flask
 
 app = Flask(__name__)
 
-'''
+"""Gets and prints the spreadsheet's header columns
 
-funcao que soma dois elementos
+Parameters
+----------
+file_loc : str
+    The file location of the spreadsheet
+print_cols : bool, optional
+    A flag used to print the columns to the console (default is False)
 
-'''
+Returns
+-------
+list
+    a list of strings representing the header columns
+"""
 
 def soma(termo1,termo2):
 
     return termo1+termo2
 
 
-'''
+"""Gets and prints the spreadsheet's header columns
 
-funcao raiz da minha aplicacao web
+Parameters
+----------
+file_loc : str
+    The file location of the spreadsheet
+print_cols : bool, optional
+    A flag used to print the columns to the console (default is False)
 
-'''
-
+Returns
+-------
+list
+    a list of strings representing the header columns
+"""
 @app.route("/")
 def hello():
 
